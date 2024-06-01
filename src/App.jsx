30eedbe891b93c53c14c44748ef501fcc9ce83a6
steve <austines811@gmail.com>
2024-06-01 04:42:56 +0300

    creates placeholder routes for app router in dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import react from "react";
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./components/auth/SignIn";
 import "./App.css";
-
+import NavigationBar from "./components/navs/NavigationBar";
+import { Typography } from "@mui/material";
 function App() {
   return (
-    <Routes>
-      {" "}
-      <Route path="/" element={<SignIn />}></Route>
-      <Route path="dashboard" element={<>Dahboard!</>} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        {" "}
+        <Route path="/" element={<SignIn />}></Route>
+        <Route path="agent" element={<NavigationBar/>}>
+        <Route path="dashboard" element={<Typography>Dashboard</Typography>} />
+        <Route path="schools" element={<Typography>Schools</Typography>} ></Route>
+        <Route path="invoices" element={<Typography>Invoices</Typography>}></Route>
+        <Route path="collections" element={<Typography>Collections</Typography>}></Route>
+      
+        </Route>
+        </Routes>
+    </div>
   );
 }
 
