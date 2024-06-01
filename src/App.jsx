089f75c736c14b5688758c0ccbaf1d77761e31a3
steve <austines811@gmail.com>
2024-06-01 06:51:32 +0300

    creates routes for school view and enrolment

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,34 @@ import SignIn from "./components/auth/SignIn";
 import "./App.css";
 import NavigationBar from "./components/navs/NavigationBar";
 import { Typography } from "@mui/material";
+import SchoolsNav from "./components/navs/SchoolsNav";
+import CreateSchoolForm from "./components/forms/CreateSchoolForm";
+import OnboardedSchoolsTable from "./components/tables/OnboardedSchoolsTable";
 function App() {
   return (
     <div className="App">
       <Routes>
         {" "}
         <Route path="/" element={<SignIn />}></Route>
-        <Route path="agent" element={<NavigationBar/>}>
-        <Route path="dashboard" element={<Typography>Dashboard</Typography>} />
-        <Route path="schools" element={<Typography>Schools</Typography>} ></Route>
-        <Route path="invoices" element={<Typography>Invoices</Typography>}></Route>
-        <Route path="collections" element={<Typography>Collections</Typography>}></Route>
-      
+        <Route path="agent" element={<NavigationBar />}>
+          <Route
+            path="dashboard"
+            element={<Typography>Dashboard</Typography>}
+          />
+          <Route path="schools" element={<SchoolsNav homepage={"agent"} />}>
+            <Route index element={<OnboardedSchoolsTable />} />
+            <Route path="enrol" element={<CreateSchoolForm />} />
+          </Route>
+          <Route
+            path="invoices"
+            element={<Typography>Invoices</Typography>}
+          ></Route>
+          <Route
+            path="collections"
+            element={<Typography>Collections</Typography>}
+          ></Route>
         </Route>
-        </Routes>
+      </Routes>
     </div>
   );
 }
