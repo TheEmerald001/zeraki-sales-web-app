5e0ee24e4b6285c739ac2310c51f02d2266a806f
steve <austines811@gmail.com>
2024-06-02 17:28:54 +0300

    updates grid utility for style and id requirement

--- src/components/utilities/CustomDataGrid.jsx
+++ src/components/utilities/CustomDataGrid.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { GridToolbarQuickFilter } from "@mui/x-data-grid";
-import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import CustomNoRowsOverlay from "./CustomNoRowsOverlay.jsx";
 
 const CustomDataGrid = ({ rows, columns }) => {
   const [pageSize, setPageSize] = useState(25);
@@ -20,26 +16,14 @@ const CustomDataGrid = ({ rows, columns }) => {
         }}
       >
         <DataGrid
-          getRowId={(row) => row.code || row.id || row.receipt_number || row.user_ref}
+          getRowId={(row) => row.id}
           rows={rows}
           columns={columns}
-          autoHeight  
-          sx={{ mt: -3, p: 2 }}        
+          autoHeight
+          sx={{ mt: -3, p: 2, m: 1 }}
           pageSize={pageSize}
           rowsPerPageOptions={[25, 50, 100, 200, 400]}
           onPageSizeChange={(newValue) => setPageSize(newValue)}
-          components={{
-            ColumnArrowDropDownIcon: ArrowDropDownIcon,
-            ColumnArrowDropUpIcon: ArrowDropUpIcon,
-            NoRowsOverlay: CustomNoRowsOverlay,
-            Toolbar: GridToolbarQuickFilter,
-          }}
-          componentsProps={{
-            toolbar: {
-              showQuickFilter: true,
-              quickFilterProps: { debounceMs: 500 },
-            },
-          }}
         />
       </Box>
     </>
