2ceac1ecb618e4349f3495c3f2b64075fb98df08
steve <austines811@gmail.com>
2024-06-02 23:08:44 +0300

    creates an AppBar for ease of interaction on large screens

---
+++ src/components/navs/UserAppBar.jsx
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../features/user/userSlice.js";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { Avatar } from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Person_Avatar from "../../images/Person_Avatar.png";
+import UserAccountModal from "../modals/UserAccountModal.jsx";
+import { store } from "../../store/store.js";
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function UserAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  const [openUserAccountModal, setOpenUserAccountModal] = useState(false);
+  const user = useSelector(selectCurrentUser);
+  const handleUserAccount = () => {
+    handleMenuClose();
+    setOpenUserAccountModal(true);
+  };
+  const logOut = () => {
+    return {
+      type: "USER_LOGOUT",
+    };
+  };
+  const handleLogOut = () => {
+    handleMenuClose();
+    store.dispatch(logOut());
+  };
+
+  const closeUserAccountModal = () => {
+    setOpenUserAccountModal(false);
+  };
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      sx={{ mt: 8 }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleUserAccount}>My Profile</MenuItem>
+      <MenuItem onClick={handleLogOut}>Log Out</MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box>
+      <UserAccountModal
+        openUserAccountModal={openUserAccountModal}
+        closeUserAccountModal={closeUserAccountModal}
+      />
+      <AppBar position="fixed" sx={{ boxShadow: 0, left: "240px", right: 0 }}>
+        <Toolbar
+          sx={{
+            // color: "black",
+            // backgroundColor: "white",
+            p: 2,
+          }}
+        >
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Global Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 0.75 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <Avatar alt="S-Admin" src={Person_Avatar} sizes="150px" />
+            </IconButton>
+            <Box sx={{ ml: 1, textAlign: "right" }}>
+              <Typography
+                sx={{ fontSize: "1rem", fontWeight: 400, color: "white" }}
+              >
+                {user.user_name}
+              </Typography>
+              <Typography
+                sx={{ fontSize: "0.9rem", fontWeight: 200, color: "white" }}
+              >
+                Sales
+              </Typography>
+            </Box>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </Box>
+  );
+}
