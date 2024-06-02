3f7169238acd6cf5892c3c2a6361f15c39cac372
steve <austines811@gmail.com>
2024-06-02 21:46:26 +0300

    cleans directives in api file on completion

--- src/api/metrics/metrics.js
+++ src/api/metrics/metrics.js
@@ -1,25 +1,11 @@
-import moduleAxios from "../axios";
+import { moduleAxios } from "../axios";
 
-// implements apis that return data from the server
-// A. Top Card Metrics
-// i. Total collections made
+// i. Fetch total collections made
 export const getCollections = async () => {
-    return await moduleAxios.get(`/collections`);
+  return await moduleAxios.get(`/collections`);
 };
-// ii. Show total signs ups and per product signups - utilize the schools api
-// iii. Show total revenue collected and per product revenue - utilize collections
-// iv. Show bounced cheques - utilize invoices and filter status
-// v. show failed collections - utilize collections
 
-
-// B. Target Visualization
-// i. show target data per product versus achievement utilize schools
-
-// C. Signups overview
-// i. display signup per type of school per product - utilize schools
-
-// D. Upcoming Invoices
-// Display invoice list due soon
-export const getDueCollections = async () => {
-    return await moduleAxios.get(`/invoices?_limit=10`);
+// Fetch due Invoices
+export const getInvoices = async () => {
+  return await moduleAxios.get(`/invoices`);
 };
